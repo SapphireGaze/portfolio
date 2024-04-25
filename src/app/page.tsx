9c4153c37eeab1c28c746500ae8df929c4ab6415
SapphireGaze <jasminehuang2048@outlook.com>
2024-04-25 00:51:52 -0700

    Github Pages Config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src="/home/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
