f8200ce336d77a2358cd203aed9da9ed8e695ec8
SapphireGaze <jasminehuang2048@outlook.com>
2024-06-30 02:35:13 -0700

    Added MUI & Themes:  - Added Material UI library  - Created default theme and ThemeProviderWrapper  - Removed boilerplate CSS

---
+++ src/themes/default.ts
@@ -0,0 +1,34 @@
+import { Theme, ThemeOptions, createTheme } from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#6B705C",
+    },
+    secondary: {
+      main: "#3F4238",
+    },
+    background: {
+      default: "#B7B7A4",
+      paper: "#D4C7B0",
+    },
+  },
+};
+
+const defaultTheme: Theme = createTheme(themeOptions);
+
+export default defaultTheme;
